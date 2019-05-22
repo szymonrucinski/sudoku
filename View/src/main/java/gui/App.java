88a03ec2305f123d01ce7d81093767b25e429354
217860 Ruciński Szymon <217860@edu.p.lodz.pl>
2019-05-22 12:37:44 +0200

    Środa 22maja - JavaFx workd, modules visable
    Środa 22maja - JavaFx workd, modules visable

--- View/src/main/java/gui/App.java
+++ View/src/main/java/gui/App.java
@@ -1,4 +1,6 @@
 package gui;
+import comprog.SudokuBoard;
+
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -11,6 +13,8 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("My First JavaFX App");
 
+
+        SudokuBoard Sudoku = new SudokuBoard();
         Label label = new Label("Twoja Mama!");
         Scene scene = new Scene(label, 400, 200);
         primaryStage.setScene(scene);
