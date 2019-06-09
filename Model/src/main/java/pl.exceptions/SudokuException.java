93b572f785a8692cf60a5621a456d256681cba29
217860 Ruciński Szymon <217860@edu.p.lodz.pl>
2019-06-09 19:52:27 +0200

    WORKING ON EXCEPTIONS

---
+++ Model/src/main/java/pl.exceptions/SudokuException.java
@@ -0,0 +1,12 @@
+package pl.exceptions;
+
+public class SudokuException extends Exception {
+
+    public SudokuException(String message) {
+        super(message);
+    }
+
+    public SudokuException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
